4d8634fbaf7cd7fccf7cb82731af2a10dd380702
Furkan <furkan68mutlu@gmail.com>
2022-12-11 21:41:04 +0300

    restarting

--- lib/core/models/network/response_model.dart
+++ lib/core/models/network/response_model.dart
@@ -1,19 +1,13 @@
 import 'package:dio/dio.dart';
-import 'package:finance_app/core/base/model/base_error_model.dart';
 import 'package:finance_app/core/base/model/base_response_model.dart';
 
 class ResponseModel<T> extends BaseResponseModel<T> {
   final Headers? headers;
   ResponseModel({
     this.headers,
-    required int statusCode,
-    required String statusMessage,
-    T? data,
-    BaseError? error,
-  }) : super(
-          statusCode: statusCode,
-          statusMessage: statusMessage,
-          data: data,
-          error: error,
-        );
+    required super.statusCode,
+    required super.statusMessage,
+    super.data,
+    super.error,
+  });
 }
